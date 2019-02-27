90e8fef3297b52207706539df0f8c80ff2fc646e
rudywaltz <david.barkoczi@gmail.com>
2019-02-27 17:14:42 +0100

    feat(player)fix(audio): start/stop  song #6

--- cypress/integration/player.js
+++ cypress/integration/player.js
@@ -1,28 +1,15 @@
 const getStore = () => cy.window().its('store')
 
+const expectPlayingAudio = (elem, assert = true) => {
+  elem.should(el => {
+    const audible = el[0].duration > 0 && !el[0].paused && !el[0].muted;
+    expect(audible).to.eq(assert);
+  })
+
+
 describe('player', () => {
   beforeEach(() => {
     cy.visit('/')
-  });
-
-  it('should be render', () => {
-    cy.get('#player');
-  });
-
-  it('audio', () => {
-    cy.get('#player audio');
-  });
-
-  it('render song url', () => {
-    cy
-      .get('#player .player__title')
-      .contains('/jezusesajelzoraketa.mp3')
-  });
-
-  it('render song duration', () => {
-    cy
-      .get('#player .player__duration')
-      .contains('00:05:46')
   });
 
   context('without song', () => {
@@ -44,4 +31,37 @@ describe('player', () => {
         .contains('--:--:--')
     });
   })
+
+  it('should be render', () => {
+    cy.get('#player');
+  });
+
+  it('audio', () => {
+    cy.get('#player audio');
+  });
+
+  it('render song url', () => {
+    cy
+      .get('#player .player__title')
+      .contains('/jezusesajelzoraketa.mp3')
+  });
+
+  it('render song duration', () => {
+    cy
+      .get('#player .player__duration')
+      .contains('00:05:46')
+  });
+
+  it('play sound', () => {
+    cy.get('#player .player__play')
+      .click()
+    expectPlayingAudio(cy.get('audio'), true);
+  });
+
+  it('stop sound', () => {
+    cy.get('#player .player__play')
+      .click()
+      .click()
+      expectPlayingAudio(cy.get('audio'), false);
+  });
 });
