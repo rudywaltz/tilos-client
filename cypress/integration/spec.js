eefd09576b507e7fc1352b6f8d560bd47ab4b678
rudywaltz <david.barkoczi@gmail.com>
2019-02-27 12:09:46 +0100

    docs(readme): remove default content from index page #3

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,10 +3,6 @@ describe('Tilos-client-app', () => {
     cy.visit('/')
   });
 
-  it('has the correct <h1>', () => {
-    cy.contains('h1', 'Great success!')
-  });
-
   it('navigates to /about', () => {
     cy.get('nav a').contains('about').click();
     cy.url().should('include', '/about');
