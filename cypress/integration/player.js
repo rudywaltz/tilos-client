ecff6d6ece8cf6bbea4ad053eb5cc3296a3ff06c
rudywaltz <david.barkoczi@gmail.com>
2019-03-03 08:28:21 +0100

    test(player): re organize tests #7

--- cypress/integration/player.js
+++ cypress/integration/player.js
@@ -26,55 +26,64 @@ describe('player', () => {
     });
   })
 
-  it('should be render', () => {
-    cy.get('#player');
-  });
-
-  it('render song url', () => {
-    cy
-      .get('#player .player__title')
-      .contains('/jezusesajelzoraketa.mp3')
-  });
-
-  it('render song duration', () => {
-    cy
-      .get('#player .player__duration')
-      .contains('00:05:46')
-  });
-
-  it('play sound', () => {
-    cy.get('#player .player__play')
-      .click();
-      cy.get('#player .player__play').contains('Pause')
-  });
-
-  it('stop sound', () => {
-    cy.get('#player .player__play')
-      .click()
-      .click()
-    cy.get('#player .player__play').contains('Play')
-  });
-
-  it('has progressbar', () => {
-    cy.get('#player .progress .progress__bar')
-  });
-
-  it('render current time', () => { // TODO: more accurate test
-    cy.get('#player .player__play')
-      .click()
-    cy.get('#player .player__current')
-    .contains('00:00:01')
-    cy.get('#player .player__current')
-    .contains('00:00:02')
-    cy.get('.progress__bar')
-      .should( $div => {
-         expect($div[0].style.width).to.be.greaterThan('0.30%');
+  context('with song', () => {
+    beforeEach(() => {
+      cy.setStorage('song', {
+        title: 'Jézus és a jelzőrakéta',
+        url: '/jezusesajelzoraketa.mp3'
       })
-  });
-
-  it.skip('render current time2', () => { // TODO
-    cy.get('audio').should($element => {
-      $element[0].currentTime = 100;
     })
+
+    it('should be render', () => {
+      cy.get('#player');
+    });
+
+    it('render song url', () => {
+      cy
+        .get('#player .player__title')
+        .contains('/jezusesajelzoraketa.mp3')
+    });
+
+    it('render song duration', () => {
+      cy
+        .get('#player .player__duration')
+        .contains('00:05:46')
+    });
+
+    it('play sound', () => {
+      cy.get('#player .player__play')
+        .click();
+        cy.get('#player .player__play').contains('Pause')
+    });
+
+    it('stop sound', () => {
+      cy.get('#player .player__play')
+        .click()
+        .click()
+      cy.get('#player .player__play').contains('Play')
+    });
+
+    it('has progressbar', () => {
+      cy.get('#player .progress .progress__bar')
+    });
+
+    it('render current time', () => { // TODO: more accurate test
+      cy.get('#player .player__play')
+        .click()
+      cy.get('#player .player__current')
+      .contains('00:00:01')
+      cy.get('#player .player__current')
+      .contains('00:00:02')
+      cy.get('.progress__bar')
+        .should( $div => {
+          expect($div[0].style.width).to.be.greaterThan('0.30%');
+        })
+    });
+
+    it.skip('render current time2', () => { // TODO
+      cy.get('audio').should($element => {
+        $element[0].currentTime = 100;
+      })
+    });
   });
 });
