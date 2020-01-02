957866bb0602c2d9dd3d21a9449efd5c5a3fb56d
rudywaltz <david.barkoczi@gmail.com>
2020-01-02 08:03:33 +0100

    style(archive): alphabetical order

--- src/routes/archive/[slug].svelte
+++ src/routes/archive/[slug].svelte
@@ -31,12 +31,13 @@
 
   $: episodes = episodes.map(episode => {
     return {
+      alias: episode.show.alias,
+      duration: (episode.realTo - episode.realFrom) / 1000,
+      inThePast: episode.inThePast,
+      mp3: episode.m3uUrl ? episode.m3uUrl.slice(0, -3) + 'mp3' : '',
       name: episode.show.name,
       showId: episode.show.id,
-      inThePast: episode.inThePast,
-      text: episode.text ? episode.text.title : '------',
-      mp3: episode.m3uUrl ? episode.m3uUrl.slice(0, -3) + 'mp3' : '',
-      duration: (episode.realTo - episode.realFrom) / 1000
+      text: episode.text ? episode.text.title : '------'
     };
   });
 
